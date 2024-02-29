820b36b58142203215e1cd7eb20d7c5aa54d0e4e
amedina12 <amedina12@hawk.iit.edu>
2024-02-29 03:32:21 -0600

    adding flashing javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const span = document.getElementById('flashingText');
+
+    setInterval(() => {
+        span.style.visibility = (span.style.visibility === 'hidden' ? 'visible' : 'hidden');
+    }, 700); // Toggle every 500 milliseconds (half a second)
+});
 
